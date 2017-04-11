e447d6c3a1cbf313bc5f232b03f474c9e20edfe0
WangYihang <wangyihanger@gmail.com>
2017-04-11 11:08:22 +0000

    add getstart button event

--- application/views/home/view.php
+++ application/views/home/view.php
@@ -2,6 +2,6 @@
 	<h1 class="cover-heading">Capture The Flag</h1>
 	<p class="lead">SniperOJ is an open source and free CTF online judge system , now it is developing... If you have any questions , please contact this email : admin@sninperoj.cn</p>
 	<p class="lead">
-		<a href="#" class="btn btn-lg btn-default">Get start</a>
+		<a href="/user/login" class="btn btn-lg btn-default">Get start</a>
 	</p>
 </div>
