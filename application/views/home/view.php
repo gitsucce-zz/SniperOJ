a97368ddb98eaa0835fd4c514de9cba045195afa
WangYihang <wangyihanger@gmail.com>
2017-04-12 21:08:39 +0000

    almost finished

--- application/views/home/view.php
+++ application/views/home/view.php
@@ -1,8 +1,12 @@
 <div class="home-content">
 	<h1 class="cover-heading">Capture The Flag</h1>
-	<p class="lead">SniperOJ is an open source and free CTF online judge system , now it is developing... If you have any questions , please join in the QQ group : 514782018</p>
-	Github : https://github.com/WangYihang/SniperOJ<br>
-	Blog : http://www.jianshu.com/u/bf30f18c872c<br>
+	<p class="lead">
+		SniperOJ is an open source and free CTF online judge system<br>
+		This system is currently under development now<br>
+		if you have any suggestions or comments or questions<br>
+		you can join the QQ group (514782018) to discuss<br>
+		or click the email button below the page to contact with me<br>
+		And your question will be quickly processed.
 	<a href="https"></a>
 	<p class="lead">
 		<a href="/user/login" class="btn btn-lg btn-default">Get start</a>
--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -2,7 +2,11 @@
     
     <nav class="navbar navbar-inverse navbar-fixed-bottom" role="navigation">
       <em>&copy; 2017</em><br>
-      Contact : admin@sniperoj.cn
+      <a target="_black" href="https://github.com/WangYihang/SniperOJ">Github</a>
+      &nbsp;&nbsp;
+      <a target="_black" href="http://www.jianshu.com/u/bf30f18c872c">Blog</a>
+      &nbsp;&nbsp;
+      <a target="_black" href="admin@sniperoj.cn">Email</a>
     </nav>
 
 
