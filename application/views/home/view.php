3fb7516cc5a71a584dc5183aed2381217e5574b3
WangYihang <wangyihanger@gmail.com>
2017-04-12 14:23:17 +0000

    add \n

--- application/views/home/view.php
+++ application/views/home/view.php
@@ -1,8 +1,8 @@
 <div class="inner cover">
 	<h1 class="cover-heading">Capture The Flag</h1>
 	<p class="lead">SniperOJ is an open source and free CTF online judge system , now it is developing... If you have any questions , please join in the QQ group : 514782018</p>
-	Github : https://github.com/WangYihang/SniperOJ
-	Blog : http://www.jianshu.com/u/bf30f18c872c
+	Github : https://github.com/WangYihang/SniperOJ<br>
+	Blog : http://www.jianshu.com/u/bf30f18c872c<br>
 	<a href="https"></a>
 	<p class="lead">
 		<a href="/user/login" class="btn btn-lg btn-default">Get start</a>
